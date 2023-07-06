ad40fc86b27351c852fb7f620b9c3e8572609d11
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-07-06 08:30:21 +0000

    chore(deps): Updated

--- src/actions-organization-variable/index.ts
+++ src/actions-organization-variable/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/5.29.0/docs/resources/actions_organization_variable
 // generated from terraform resource schema
 
