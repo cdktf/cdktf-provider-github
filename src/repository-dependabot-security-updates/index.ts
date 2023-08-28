f31de2eab3ffc90c76ea8bf7323dac06c50c5b71
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-08-28 10:06:56 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/repository-dependabot-security-updates/index.ts
+++ src/repository-dependabot-security-updates/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/integrations/github/5.34.0/docs/resources/repository_dependabot_security_updates
 // generated from terraform resource schema
 
