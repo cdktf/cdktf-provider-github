2996a7d870db2d3766e3e3046b18d02799d05585
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-01 12:28:51 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-github-actions-organization-oidc-subject-claim-customization-template/index.ts
+++ src/data-github-actions-organization-oidc-subject-claim-customization-template/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/integrations/github/5.34.0/docs/data-sources/actions_organization_oidc_subject_claim_customization_template
 // generated from terraform resource schema
 
