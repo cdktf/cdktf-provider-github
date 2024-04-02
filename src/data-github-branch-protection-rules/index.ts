aabf8534b294dbe78be58aa501b19dfcf78a7065
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-04-02 13:29:28 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@c6606f6

--- src/data-github-branch-protection-rules/index.ts
+++ src/data-github-branch-protection-rules/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.2.1/docs/data-sources/branch_protection_rules
 // generated from terraform resource schema
 
