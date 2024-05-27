ab648b5fe5eceb3166d45fb4c11c51fa509e7710
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-05-27 09:31:25 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@af4e734

--- src/data-github-repository-autolink-references/index.ts
+++ src/data-github-repository-autolink-references/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.2.1/docs/data-sources/repository_autolink_references
 // generated from terraform resource schema
 
