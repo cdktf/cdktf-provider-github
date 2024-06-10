9623c2a50a76a612d44fa1fabab64270f727fe2f
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-06-10 07:54:10 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@6ef397a

--- src/issue-label/index.ts
+++ src/issue-label/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.2.1/docs/resources/issue_label
 // generated from terraform resource schema
 
