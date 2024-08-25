592dfcca50ec199b0970456617c538f83de8df9a
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-25 05:57:12 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@ffdc145

--- src/data-github-repository-webhooks/index.ts
+++ src/data-github-repository-webhooks/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.2.3/docs/data-sources/repository_webhooks
 // generated from terraform resource schema
 
