340af31e8a5f5e11420331dbc6a637b34d95a61b
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-14 03:41:10 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/user-invitation-accepter/index.ts
+++ src/user-invitation-accepter/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/integrations/github/6.3.0/docs/resources/user_invitation_accepter
 // generated from terraform resource schema
 
