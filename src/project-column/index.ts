ff42269c9d8aac0902aa809912f5c7f1f0dc69d8
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-10-27 05:58:32 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@555b856

--- src/project-column/index.ts
+++ src/project-column/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.3.1/docs/resources/project_column
 // generated from terraform resource schema
 
