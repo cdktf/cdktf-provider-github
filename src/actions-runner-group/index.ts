fece431c2f78aed36c56f681e3602dcb137202cc
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-17 05:59:45 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@f7e4e9f

--- src/actions-runner-group/index.ts
+++ src/actions-runner-group/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.3.1/docs/resources/actions_runner_group
 // generated from terraform resource schema
 
