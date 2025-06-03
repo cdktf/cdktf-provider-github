da1fc2ce8f6849d3a931cba112ec9b0d07b8fa0d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:38:16 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@909bd84

--- src/organization-security-manager/index.ts
+++ src/organization-security-manager/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/integrations/github/6.6.0/docs/resources/organization_security_manager
 // generated from terraform resource schema
 
