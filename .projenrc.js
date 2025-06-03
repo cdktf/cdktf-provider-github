a29831d7814496bff382de519eb3aae27585f059
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2025-06-03 14:13:13 +0200

    chore(deps): update  by cdktf/cdktf-repository-manager@4c005a5 (#1910)
    Triggered by
    https://github.com/cdktf/cdktf-repository-manager/actions/runs/15416575556
    
    ---------
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- .projenrc.js
+++ .projenrc.js
@@ -5,7 +5,6 @@
 
 const { CdktfProviderProject } = require("@cdktf/provider-project");
 
-const typescriptVersion = "~5.4.0";
 const project = new CdktfProviderProject({
   useCustomGithubRunner: false,
   terraformProvider: "integrations/github@~> 6.0",
